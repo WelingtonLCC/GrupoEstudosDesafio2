1465d477a19a7d28ee0a0858ffd176253c709673
jefferson8808 <jefferson.spm8808@gmail.com>
2024-02-12 19:23:40 -0300

    adicinado pagina historico

---
+++ SlnImc/BlazorImc/ViewModel/Historico.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorImc.ViewModel
+{
+    public class Historico
+    {
+        public bool PesoIdeal { get; set; }
+        public string DataTempo { get; set; } = string.Empty;
+        public string TextoResultado { get; set; } = string.Empty;
+    }
+}
