bbcba6b1b9f182b18b20a9d7e9f2be5d5be769b5
Welington <welington.c.campos@hotmail.com>
2024-02-12 22:16:42 -0300

    hhhhh

--- SlnImc/BlazorImc/ViewModel/Imc.cs
+++ SlnImc/BlazorImc/ViewModel/Imc.cs
@@ -6,10 +6,10 @@ namespace BlazorImc.ViewModel
     {
         [Required]
         [Range(0.0001, double.MaxValue, ErrorMessage = "a altura deve ser maior que zero.")]
-        public double Altura { get; set; }
+        public double? Altura { get; set; }
         [Required]
         [Range(0.0001, double.MaxValue, ErrorMessage = "o peso deve ser maior que zero.")]
-        public double Peso { get; set; }
+        public double? Peso { get; set; }
         [Required]
         [MinLength(8, ErrorMessage = "O Sexo deve ter pelo menos 8 caracteres.")]
         [MaxLength(9, ErrorMessage = "O Sexo deve ter no máximo 19 caracteres.")]
